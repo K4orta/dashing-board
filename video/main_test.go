74ff718a0f554a30af92fb342a984cc9287969b2
Erik Wong <ewong@blizzard.com>
2016-01-25 08:48:51 -0800

    Add test for new youtube link style

---
+++ video/main_test.go
@@ -0,0 +1,11 @@
+package video
+
+import "testing"
+
+func TestVideoParse(t *testing.T) {
+	testString := `http://www.youtube.com/attribution_link?a=oehqRwd27Ok&u=/watch%3Fv%3Dlse2Lix6j3k%26feature%3Dem-share_video_user`
+	resultID := "oehqRwd27Ok"
+	if val := parseVideoString(testString); val != resultID {
+		t.Error("Parsing error", val)
+	}
+}
